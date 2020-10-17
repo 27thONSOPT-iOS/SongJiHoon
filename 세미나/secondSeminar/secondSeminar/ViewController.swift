25d70d4e418eb06b88bbe2993098852e24cae484
i-colours-u <i.colours.u@gmail.com>
2020-10-18 01:30:28 +0900

    [ADD] 2주차 세미나 자료 업로드

---
+++ 세미나/secondSeminar/secondSeminar/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  secondSeminar
+//
+//  Created by 송지훈 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+}
+
