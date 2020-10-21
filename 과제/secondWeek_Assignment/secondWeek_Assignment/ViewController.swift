1b6abca089819e2aa8597619e96ccccbc989306a
i-colours-u <i.colours.u@gmail.com>
2020-10-21 11:40:37 +0900

    [ADD] 2주차 과제 업로드

---
+++ 과제/secondWeek_Assignment/secondWeek_Assignment/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  secondWeek_Assignment
+//
+//  Created by 송지훈 on 2020/10/21.
+//
+
+import UIKit
+
+
+
+extension UIScrollView {
+    func scrollToTop() {
+        let desiredOffset = CGPoint(x: 0, y: -contentInset.top)
+        setContentOffset(desiredOffset, animated: true)
+   }
+}
+class ViewController: UIViewController ,UIScrollViewDelegate{
+
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    @IBOutlet weak var upButton: UIButton!
+    @IBOutlet weak var bannerImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        upButton.isHidden = true
+        self.mainScrollView.delegate = self
+    }
+    
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let yPosition = scrollView.contentOffset.y
+        
+    
+        if yPosition > self.bannerImageView.bounds.height
+        {
+            upButton.isHidden = false
+        }
+        else
+        {
+            
+            upButton.isHidden = true
+        }
+    }
+    
+
+    @IBAction func upButtonClicked(_ sender: Any) {
+        
+        mainScrollView.scrollToTop()
+    }
+    
+}
+
